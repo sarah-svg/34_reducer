b108fe533acd932cb6851ba82154f66dac9d2f65
sarah-svg <kjskillen@gmail.com>
2021-02-16 14:03:38 -0800

    test 1 for the current color

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -38,9 +38,9 @@ function App() {
     <>
       <button onClick={undo}>undo</button>
       <button onClick={redo}>redo</button>
-      <input type="color" value={current}
+      <input data-testid="current" type="color" value={current}
         onChange={({ target }) => record(target.value)} />
-      <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}>
+      <div  data-testid="display" style={{ backgroundColor: current, width: '10rem', height: '10rem' }}>
       </div>
     </>
   );
--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,32 @@
+  
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
+  
+  it('takes a snapshot test of App', async() => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
+
+  });
+
+  it('checks the current color', async() => {
+    render(<App />);
+
+    const current = screen.getByTestId('current');
+    
+    fireEvent.change(current, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+
+    const display = await screen.findByTestId('display');
+        
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000',
+    });
   });
 });
